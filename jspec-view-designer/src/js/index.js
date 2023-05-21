e7fb2f59f5cb87792c6f131c0f2e40b85a4280e7
kedama <uesastorage3@gmail.com>
2023-05-21 19:35:14 +0900

    empty

--- jspec-view-designer/src/js/index.js
+++
@@ -1,9 +0,0 @@
-const Vue = require('vue');
-import {jspecViewDesigner} from './jspec-view-designer.js';
-
-window.app = Vue.createApp({
-  components: {
-    "jspec-view-designer": jspecViewDesigner
-  },
-}).mount('#app');
-
