1553b8f6a3d2f2c6e5a4f28a2dfac9bdce6aaf95
kedama <uesastorage3@gmail.com>
2024-03-31 20:55:49 +0900

    impl

--- jspec-app/codegen/codegen-generic/codegen-generic.js
+++ jspec-app/codegen/codegen-generic/codegen-generic.js
@@ -1,9 +1,12 @@
 const { createApp } = Vue;
+
+function $key() { return app.key };
+
 let app = createApp({
   data() {
     return {
       source: `{ "x" : "foo" }`,
-      template: "${__key__} www ${x} zzz",
+      template: "${$key} www ${x} zzz",
       key: "key1",
     };
   },
@@ -11,7 +14,6 @@ let app = createApp({
     code() {
       try {
         let env = JSON.parse(this.source);
-        env["__key__"] = this.key;
         return (new Function(...Object.keys(env), "return " + "`" + this.template + "`")).call(env, ...Object.values(env));
       } catch(ex) {
         console.log(ex);
