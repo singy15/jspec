258d559e5072f711f497f59d7f1d77f6832f5273
kedama <uesastorage3@gmail.com>
2025-09-22 22:25:06 +0900

    impl

--- jspec-editor.re/jspec-editor.re/jspec.proxy.js
+++ jspec-editor.re/jspec-editor.re/jspec.proxy.js
@@ -1,3 +1,6 @@
+
+// @@@webenv.script(main)
+
 const jspec = (obj) => {
   const isObject = (val) => {
     return val != null && val !== undefined && val.constructor == Object;
@@ -19,25 +22,64 @@ const jspec = (obj) => {
     return val;
   };
 
-  const createHandler = (root) => {
+  const createHandler = (root, linker = null) => {
     return {
       get(target, prop, receiver) {
-        let ret;
+        let ret = undefined;
 
-        if (isObject(target)) {
-          ret = target[prop];
-        } else if (isArray(target)) {
-          ret = target[parseInt(prop, 10)];
+        ret = linker?.[prop];
+
+        if (ret === undefined) {
+          if (isObject(target)) {
+            ret = target[prop];
+          } else if (isArray(target)) {
+            ret = target[parseInt(prop, 10)];
+          }
         }
 
+        let resolved = null;
         if (isObject(ret) && ret.$link) {
+          resolved = ret;
           ret = resolve(root, ret.$link);
         }
 
         if (isObject(ret) || isArray(ret))
-          ret = new Proxy(ret, createHandler(root));
+          ret = new Proxy(ret, createHandler(root, resolved));
 
         return ret;
+      },
+      set(target, prop, value) {
+        if (linker != null && prop in linker) {
+          linker[prop] = value;
+        } else {
+          if (isObject(target)) {
+            target[prop] = value;
+          } else if (isArray(target)) {
+            target[parseInt(prop, 10)] = value;
+          }
+        }
+        return value;
+      },
+      ownKeys(target) {
+        let keys = {};
+        if (linker != null) {
+          for (let key in linker) {
+            keys[key] = true;
+          }
+        }
+        for (let key in target) {
+          keys[key] = true;
+        }
+        return Object.keys(keys);
+      },
+      getOwnPropertyDescriptor(target, prop) {
+        if (prop === "$link") {
+          return { configurable: true, enumerable: false, value: linker[prop] };
+        }
+        if (linker?.[prop] !== undefined) {
+          return { configurable: true, enumerable: true, value: linker[prop] };
+        }
+        return { configurable: true, enumerable: true, value: target[prop] };
       },
     };
   };
@@ -45,3 +87,37 @@ const jspec = (obj) => {
   return new Proxy(obj, createHandler(obj));
 };
 
+const jspecObj = {
+  simple: {
+    prop1: 111,
+    prop2: {
+      x: 222,
+    },
+    prop3: [333],
+    prop4: 444,
+  },
+  link1: {
+    $link: "simple",
+    prop4: 4444,
+  },
+};
+
+let spec = jspec(jspecObj);
+
+console.log("test1", spec.simple.prop1 === 111);
+console.log("test link1", spec.link1.prop1 === 111, spec.link1);
+console.log("test link2", spec.link1.prop2.x === 222);
+console.log("test link3", spec.link1.prop3[0] === 333);
+spec.link1.prop3[0] = 555;
+console.log("test link4", spec.link1.prop3[0] === 555);
+spec.link1.prop2.x = 999;
+console.log("test link5", spec.link1.prop2.x === 999);
+console.log("test link6", spec.link1.prop4 === 4444, spec.link1.prop4);
+spec.link1.prop4 = 44444;
+console.log("test link7", spec.link1.prop4 === 44444, spec.link1.prop4);
+console.log("test link8", spec.link1.$link === "simple", spec.link1.$link);
+console.log(
+  "test link9",
+  Object.keys(spec.link1).length === 4,
+  Object.keys(spec.link1),
+);
