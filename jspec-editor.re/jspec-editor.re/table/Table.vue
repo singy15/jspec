389ace9f814005bbb49054aee2cbf95a3f159bbd
kedama <uesastorage3@gmail.com>
2026-04-24 07:34:21 +0900

    impl

--- jspec-editor.re/jspec-editor.re/table/Table.vue
+++ jspec-editor.re/jspec-editor.re/table/Table.vue
@@ -3,6 +3,7 @@ const ref = Vue.ref;
 const reactive = Vue.reactive;
 const defineProps = Vue.defineProps;
 const onMounted = Vue.onMounted;
+const useTemplateRef = Vue.useTemplateRef;
 
 const props = defineProps(["model", "define"]);
 
@@ -27,12 +28,20 @@ const def = reactive({
   ],
 });
 
-function recalcLeft(def) {
+function recalcGeometry(def) {
   let left = 0;
-  def.columns.forEach((col) => {
+  let totalWidth = 0;
+  def.columns.forEach((col, j) => {
     col.left = left;
     left += col.width + 1;
+
+    if (j === def.columns.length - 1) {
+      col.last = true;
+    }
+
+    totalWidth += col.width;
   });
+  def.totalWidth = totalWidth;
 }
 
 const genDrag = ref(null);
@@ -41,8 +50,37 @@ function* drag(event, columnindex) {
   let endEvent = yield;
   let dx = endEvent.x - ix;
   def.columns[columnindex].width += dx;
-  recalcLeft(def);
+  recalcGeometry(def);
   return;
+}
+
+const table = useTemplateRef("table");
+
+const visibleRange = ref([0, 0]);
+function recalcVisibleRange() {
+  const tableHeight = table.value.getBoundingClientRect().height;
+  const scrollTop = table.value.scrollTop;
+  const visibleRowCount = Math.floor(tableHeight / def.rowHeight);
+  const startIndex = Math.floor(scrollTop / def.rowHeight);
+  visibleRange.value = [
+    startIndex, //- visibleRowCount,
+    startIndex + visibleRowCount, //+ visibleRowCount,
+  ];
+}
+
+let timerRecalcVisibleRange = null;
+function delayedRecalcVisibleRange() {
+  if (timerRecalcVisibleRange) {
+    clearTimeout(timerRecalcVisibleRange);
+  }
+  timerRecalcVisibleRange = setTimeout(() => {
+    recalcVisibleRange();
+    timerRecalcVisibleRange = null;
+  }, 200);
+}
+
+function isRowInVisibleRange(rowIndex) {
+  return visibleRange.value[0] <= rowIndex && rowIndex <= visibleRange.value[1];
 }
 
 function sink(event) {
@@ -50,61 +88,68 @@ function sink(event) {
 }
 
 onMounted(() => {
-  recalcLeft(def);
+  recalcGeometry(def);
+  recalcVisibleRange();
 });
 </script>
 
 <template>
-  <div class="table">
+  <div class="table" ref="table" @scroll="delayedRecalcVisibleRange()">
+    <!-- screen -->
+    <div
+      class="screen"
+      :style="{
+        left: `0px`,
+        top: `0px`,
+        width: `${def.totalWidth + def.columns.length}px`,
+        height: `${def.rowHeight * (model.rows.length + 1)}px`,
+      }"
+    ></div>
+
     <!-- column header -->
-    <div class="row" :style="{
-      position: `sticky`,
-      top: `0px`,
-      zIndex: 20,
-    }">
-      <template v-for="(col, j) in def.columns">
+    <template v-for="(col, j) in def.columns">
+      <div
+        class="cell bl bt bb"
+        :class="{ br: col.last }"
+        :style="{
+          position: `sticky`,
+          top: `0px`,
+          zIndex: 100,
+          width: `${col.width}px`,
+          left: `${col.left}px`,
+        }"
+      >
+        <span>{{ col.name }}</span>
         <div
-          class="cell"
+          class="resizer"
+          draggable="true"
+          @dragstart="
+            genDrag = drag($event, j);
+            genDrag.next($event, j);
+          "
+          @drag="sink($event)"
+          @dragend="genDrag.next($event)"
+        ></div>
+      </div>
+    </template>
+
+    <!-- data -->
+    <template v-for="(row, i) in model.rows">
+      <template v-for="col in def.columns">
+        <div
+          v-if="isRowInVisibleRange(i)"
+          class="cell bl bt"
+          :class="{ br: col.last, bb: i === model.rows.length - 1 }"
           :style="{
             position: `absolute`,
             width: `${col.width}px`,
             left: `${col.left}px`,
-            borderTop: `none`,
-            borderBottom: `solid 1px #333`,
+            top: `${(i + 1) * def.rowHeight}px`,
           }"
         >
-          <span>{{ col.name }}</span>
-          <div
-            class="resizer"
-            draggable="true"
-            @dragstart="
-              genDrag = drag($event, j);
-              genDrag.next($event, j);
-            "
-            @drag="sink($event)"
-            @dragend="genDrag.next($event)"
-          ></div>
+          <span>{{ row[col.name] }}</span>
         </div>
       </template>
-    </div>
-
-    <!-- data -->
-    <template v-for="(row, i) in model.rows">
-      <div class="row">
-        <template v-for="col in def.columns">
-          <div
-            class="cell"
-            :style="{
-              position: `absolute`,
-              width: `${col.width}px`,
-              left: `${col.left}px`,
-              top: `${(i + 1) * def.rowHeight}px`,
-            }"
-          >
-            <span>{{ row[col.name] }}</span>
-          </div>
-        </template>
-      </div>
     </template>
   </div>
 </template>
@@ -122,19 +167,19 @@ onMounted(() => {
   -moz-osx-font-smoothing: grayscale;
 }
 
+.bl { border-left: solid 1px #333; }
+.br { border-right: solid 1px #333; }
+.bt { border-top: solid 1px #333; }
+.bb { border-bottom: solid 1px #333; }
+
 .table {
   position: relative;
   width: 100%;
   height: 100%;
-  border: solid 1px #555;
   overflow: auto;
 
   & .cell {
     display: inline-block;
-    /* border-left: solid 1px #333; */
-    border-right: solid 1px #333;
-    border-top: solid 1px #333;
-    /* border-bottom: solid 1px #333; */
     background-color: #fff;
 
     & .resizer {
@@ -146,6 +191,14 @@ onMounted(() => {
       /* background-color: red; */
       cursor: pointer;
     }
+  }
+
+  & .screen {
+    display: inline-block;
+    position: absolute;
+    /* background-color: blue;
+    opacity: 0.3;
+    z-index: 200; */
   }
 }
 
